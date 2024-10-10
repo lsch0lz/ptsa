1cb2a3f3bdcfd23325b5ab14764d34c4145a657e
lsch0lz <lscholz14@googlemail.com>
2024-10-10 11:15:06 +0200

    fix: add val loader to mimic 4 training

--- ptsa/tasks/length_of_stay/main.py
+++ ptsa/tasks/length_of_stay/main.py
@@ -1,14 +1,14 @@
 import argparse
 import os
 import torch
-from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.nn import MSELoss
+
 
 from ptsa.tasks.length_of_stay.utils import BatchGen
 from ptsa.tasks.readers import LengthOfStayReader
 from ptsa.utils.preprocessing import Discretizer, Normalizer
-from ptsa.tasks.length_of_stay.utils import metrics, utils
+from ptsa.tasks.length_of_stay.utils import utils
 
 from ptsa.models.porbabilistic.lstm import BayesianLSTM
 
@@ -27,7 +27,7 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def prepare_data(args):
+def prepare_data(args, batch_size: int):
     train_reader = LengthOfStayReader(dataset_dir=os.path.join(args.data, 'train'),
                                       listfile=os.path.join(args.data, 'train/listfile.csv'))
     val_reader = LengthOfStayReader(dataset_dir=os.path.join(args.data, 'train'),
@@ -51,14 +51,15 @@ def prepare_data(args):
     train_data_gen = BatchGen(reader=train_reader,
                               discretizer=discretizer,
                               normalizer=normalizer,
-                              batch_size=args.batch_size,
+                              batch_size=batch_size,
                               steps=None,
                               shuffle=True,
                               partition=args.partition)
+    
     val_data_gen = BatchGen(reader=val_reader,
                             discretizer=discretizer,
                             normalizer=normalizer,
-                            batch_size=args.batch_size,
+                            batch_size=batch_size,
                             steps=None,
                             shuffle=False,
                             partition=args.partition)
@@ -70,10 +71,12 @@ def train(model, train_data_gen, val_data_gen, args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    optimizer = Adam(model.parameters(), lr=args.lr)
+    lr = 0.0002055432739893699
+    num_epochs = 20
+    optimizer = Adam(model.parameters(), lr=lr)
     criterion = MSELoss()
 
-    for epoch in range(args.epochs):
+    for epoch in range(num_epochs):
         model.train()
         for i in range(train_data_gen.steps):
             batch = next(train_data_gen)
@@ -96,7 +99,7 @@ def train(model, train_data_gen, val_data_gen, args):
         with torch.no_grad():
             for i in range(val_data_gen.steps):
                 batch = next(val_data_gen)
-                x, y = batch['data']
+                x, y = batch
                 x = torch.FloatTensor(x).to(device)
                 y = torch.FloatTensor(y).to(device)
 
@@ -112,15 +115,31 @@ def train(model, train_data_gen, val_data_gen, args):
 
 def main():
     args = parse_arguments()
-    train_data_gen, val_data_gen, discretizer_header = prepare_data(args)
+    
+    batch_size = 64
+
+    train_data_gen, val_data_gen, discretizer_header = prepare_data(args, batch_size)
 
     input_dim = len(discretizer_header)
-    model = BayesianLSTM(input_dim=input_dim,
+    """model = BayesianLSTM(input_dim=input_dim,
                          hidden_dim=args.dim,
-                         output_dim=1,
+                         output_length=1,
                          batch_size=args.batch_size,
                          dropout=args.dropout,
-                         rec_dropout=args.rec_dropout)
+                         )
+    """
+
+    hidden_dim = 92
+    hidden_size_2 = 92
+    depth = 1
+    dropout = 0.4778582594500034
+ 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+ 
+    model = BayesianLSTM(input_dim, batch_size, output_length=1,
+                 hidden_dim=hidden_dim, num_layers=depth,
+                 dropout=dropout, hidden_size_2=hidden_size_2, device=device)
 
     train(model, train_data_gen, val_data_gen, args)
 
