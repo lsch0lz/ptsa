d809374dd7251a9244826faefb278773e28d6bae
lsch0lz <lscholz14@googlemail.com>
2024-10-12 10:13:05 +0200

    feat: initial deterministic lstm

---
+++ ptsa/models/deterministic/lstm.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, dropout=0.2):
+        super(LSTM, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout)
+        self.fc = nn.Linear(hidden_size, 1)
+        
+    def forward(self, x):
+        # x shape: (batch_size, sequence_length, input_size)
+        
+        # Initialize hidden state with zeros
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        
+        # Forward propagate LSTM
+        out, _ = self.lstm(x, (h0, c0))
+        
+        # Decode the hidden state of the last time step
+        out = self.fc(out[:, -1, :])
+        return out.squeeze(1)
